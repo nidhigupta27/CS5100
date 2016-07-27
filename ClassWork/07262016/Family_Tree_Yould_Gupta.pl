d1ade73d5ef227c1ca726963a9a5ca2a518344c8
Nidhi Gupta <ngupta27@ccs.neu.edu>
2016-07-26 21:58:39 -0400

    Added classwork prolog revised

--- ClassWork/07262016/Family_Tree_Yould_Gupta.pl
+++ ClassWork/07262016/Family_Tree_Yould_Gupta.pl
@@ -1,74 +1,48 @@
-'Queen Elizabeth II'.
-'Philip'.
-'Charles'.
-'Anne'.
-'Andrew'.
-'Edward'.
-'Diana'.
-'Camilla'.
-'Captain Mark Philips'.
-'Anne'.
-'Vice-Admiral Timothy Laurence'.
-'Sarah'.
-'Sophie'.
-'William'.
-'Kate'.
-'Harry'.
-'Autumn Philips'.
-'Peter'.
-'Zara'.
-'Mike'.
-'Beatrice'.
-'Eugenie'.
-'Lousie'.
-'James'.
-'George'.
-'Savannah'.
-'Isla'.
-'Mia Grace'.
-wife('Queen Elizabeth II','Philip').
+wife('Elizabeth','Philip').
 wife('Camilla','Captain Mark Philips').
 wife('Sophie','Edward').
 wife('Kate','William').
-wife('Autumn Philips','Peter Philips').
-wife('Zara Philips','Mike Tindall').
+wife('Autumn Philips','Peter').
+wife('Zara','Mike').
 wife('Sarah','Andrew').
 wife('Diana','Charles').
+wife('Anne','Captain Mark Philip').
+wife('Anne','Vice-Admiral Timothy Laurence').
+wife('Autumn Philips','Peter').
 son('Charles','Philip').
 son('Andrew','Philip').
 son('Edward','Philip').
 son('William','Charles').
 son('Harry','Charles').
-son('Peter Philips','Captain Mark Philips').
+son('Peter','Captain Mark Philips').
 son('James','Edward').
 son('George','William').
 son(Z,X):- wife(X,Y),son(Z,Y);
 daughter('Anne','Philip').
-daughter('Zara Philips','Captain Mark Philip').
+daughter('Zara','Anne').
 daughter('Beatrice','Andrew').
 daughter('Eugenie','Andrew').
 daughter('Lousie','Edward').
-daughter('Savannah','Peter Philips').
-daughter('Isla','Peter Philps').
-daughter('Mia Grace','Mike Tindall').
+daughter('Savannah','Peter').
+daughter('Isla','Peter').
+daughter('Mia Grace','Mike').
 daughter(Z,X):- wife(X,Y),daughter(Z,Y).
 husband(X,Y) :- wife(Y,X).
-child(X,Y):- son(X,Y);daughter(X,Y).
+child(X,Y):- parent(Y,X).
 spouse(X,Y):- wife(X,Y).
 spouse(X,Y):-husband(X,Y).
-parent(X,Y):-son(X,Y).
-parent(X,Y):-daughter(X,Y).
-grandchild(X,Y) :- son(X,Z),son(Z,Y);daughter(X,Z),daughter(Z,Y).
+parent(X,Y):-son(Y,X).
+parent(X,Y):-daughter(Y,X).
+grandchild(X,Y) :- child(X,Z),child(Z,Y).
+grandparent(X,Y) :- grandchild(Y,X).
 greatGrandParent(X,Y):-parent(X,W),parent(W,Z),parent(Z,Y).
-greatGrandChild(X,Y):-son(X,W),son(W,Z),son(Z,Y).
-greatGrandChild(X,Y):-daughter(X,W),daughter(W,Z),daughter(Z,Y).
-brother(X,Y):-parent(Z,X),parent(Z,Y),son(X,Z).
-sister(X,Y):-parent(Z,X),parent(Z,Y),daughter(X,Z).
+greatGrandChild(X,Y):-child(X,W),child(W,Z),child(Z,Y).
+brother(X,Y):-son(X,Z),parent(Z,X),parent(Z,Y),X\=Y.
+sister(X,Y):-parent(Z,X),parent(Z,Y),daughter(X,Z),X\=Y.
 uncle(X,Y):-parent(Z,Y),brother(X,Z).
 brotherInLaw(X,Y):-spouse(Y,Z),brother(X,Z).
 brotherInLaw(X,Y):-spouse(Y,Z),brother(W,Z),spouse(W,Y).
 sisterInLaw(X,Y):-spouse(Y,Z),sister(X,Z).
-sisterInLaw(X,Y):-spouse(Y,Z),brother(W,Z),spouse(W,Y).
-sisterInLaw(X,Y):-spouse(Y,Z),sister(W,Z),spouse(W,Y).
-neice(X,Y):-parent(Z,X),brother(Z,Y).
-neice(X,Y):-parent(Z,X),sister(Z,Y).
+sisterInLaw(X,Y):-spouse(Y,Z),brother(W,Z),spouse(X,W).
+niece(X,Y):-daughter(X,Z),brother(Z,Y).
+niece(X,Y):-daughter(X,Z),sister(Z,Y).
--- ClassWork/07262016/Question1.pl
+++ ClassWork/07262016/Question1.pl
@@ -1,10 +1,50 @@
-footpath(59,60).
-footpath(57,59).
-footpath(59,60).
+motorway(53,62).
+motorway(62,83).
+motorway(62,63).
+motorway(62,64).
+motorway(62,76).
+motorway(62,66).
+motorway(53,68).
+motorway(54,56).
+motorway(54,62).
+motorway(54,68).
+motorway(54,83).
+motorway(63,66).
+motorway(83,62).
+motorway(60,83).
+motorway(60,68).
+motorway(60,83).
+motorway(63,64).
+motorway(63,76).
+motorway(63,64).
+motorway(63,68).
+motorway(64,69).
+motorway(68,66).
+motorway(69,66).
+footpath(52,53).
+footpath(53,54).
+footpath(56,58).
 footpath(56,57).
+footpath(57,58).
+footpath(57,59).
 footpath(58,59).
-footpath(54,53).
-footpath(53,52).
-footpath(83,65).
+footpath(59,60).
 footpath(65,68).
-motorway(56,54).
+footpath(76,69).
+footpath(83,65).
+footpath(68,65).
+route(A,B):- footpath(A,B);motorway(A,B).
+route(A,B):- footpath(B,A);motorway(B,A).
+route(A,B):- move(A,B,[]).
+%%%route(A,B):- move(B,A,[]).
+
+len([],0).
+len([_|T],N) :- len(T,X),N is X+1.
+move(A,B,S):- (footpath(A,X);motorway(A,X)),
+              not(member(X,S)),
+			  (B=X ;
+			   move(X,B,[A|S])),
+			   len(S,D),
+			   D<2.
+			   
+
