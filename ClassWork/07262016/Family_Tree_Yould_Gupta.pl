4918cbcee5768c82448cfb245397a97e1cc5cb0e
Nidhi Gupta <ngupta27@ccs.neu.edu>
2016-07-26 23:38:09 -0400

    Added classwork prolog

--- ClassWork/07262016/Family_Tree_Yould_Gupta.pl
+++ ClassWork/07262016/Family_Tree_Yould_Gupta.pl
@@ -6,7 +6,7 @@ wife('Autumn Philips','Peter').
 wife('Zara','Mike').
 wife('Sarah','Andrew').
 wife('Diana','Charles').
-wife('Anne','Captain Mark Philip').
+wife('Anne','Captain Mark Philips').
 wife('Anne','Vice-Admiral Timothy Laurence').
 wife('Autumn Philips','Peter').
 son('Charles','Philip').
@@ -17,7 +17,6 @@ son('Harry','Charles').
 son('Peter','Captain Mark Philips').
 son('James','Edward').
 son('George','William').
-son(Z,X):- wife(X,Y),son(Z,Y);
 daughter('Anne','Philip').
 daughter('Zara','Anne').
 daughter('Beatrice','Andrew').
@@ -26,15 +25,16 @@ daughter('Lousie','Edward').
 daughter('Savannah','Peter').
 daughter('Isla','Peter').
 daughter('Mia Grace','Mike').
-daughter(Z,X):- wife(X,Y),daughter(Z,Y).
+child_son(Z,X):- wife(X,Y),son(Z,Y).
+child_daughter(Z,X):- wife(X,Y),daughter(Z,Y).
 husband(X,Y) :- wife(Y,X).
 child(X,Y):- parent(Y,X).
 spouse(X,Y):- wife(X,Y).
 spouse(X,Y):-husband(X,Y).
-parent(X,Y):-son(Y,X).
-parent(X,Y):-daughter(Y,X).
+parent(X,Y):-(child_son(Y,X);son(Y,X)).
+parent(X,Y):-(child_daughter(Y,X);daughter(Y,X)).
 grandchild(X,Y) :- child(X,Z),child(Z,Y).
-grandparent(X,Y) :- grandchild(Y,X).
+grandparent(X,Y) :- parent(X,Z),parent(Z,Y).
 greatGrandParent(X,Y):-parent(X,W),parent(W,Z),parent(Z,Y).
 greatGrandChild(X,Y):-child(X,W),child(W,Z),child(Z,Y).
 brother(X,Y):-son(X,Z),parent(Z,X),parent(Z,Y),X\=Y.
@@ -44,5 +44,5 @@ brotherInLaw(X,Y):-spouse(Y,Z),brother(X,Z).
 brotherInLaw(X,Y):-spouse(Y,Z),brother(W,Z),spouse(W,Y).
 sisterInLaw(X,Y):-spouse(Y,Z),sister(X,Z).
 sisterInLaw(X,Y):-spouse(Y,Z),brother(W,Z),spouse(X,W).
-niece(X,Y):-daughter(X,Z),brother(Z,Y).
-niece(X,Y):-daughter(X,Z),sister(Z,Y).
+niece(X,Y):-(child_daughter(X,Z);daughter(X,Z)),brother(Z,Y).
+niece(X,Y):-(child_daughter(X,Z);daughter(X,Z)),sister(Z,Y).
